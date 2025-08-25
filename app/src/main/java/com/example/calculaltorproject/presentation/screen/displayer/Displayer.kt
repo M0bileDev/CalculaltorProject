7370c10863fce57c215a161a842bcacc1b492018
m0bileDev <damian.ogorek.1996@gmail.com>
2025-08-25 22:01:04 +0200

    Text style update;

--- app/src/main/java/com/example/calculaltorproject/presentation/screen/displayer/Displayer.kt
+++ app/src/main/java/com/example/calculaltorproject/presentation/screen/displayer/Displayer.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.calculaltorproject.presentation.components.RoundButton
 
 @Composable
@@ -25,7 +29,11 @@ fun Displayer(
                 .padding(bottom = 72.dp),
             value = value,
             onValueChange = onValueChange,
-            readOnly = true
+            readOnly = true,
+            textStyle = LocalTextStyle.current.copy(fontSize = 24.sp),
+            supportingText = {
+                Text("test", )
+            }
         )
         RoundButton(
             size = 56.dp,
