9f183544ca3f483f0c7ae06ba55ce63e910f26a4
m0bileDev <damian.ogorek.1996@gmail.com>
2025-08-30 17:24:31 +0200

    Parameter name change;

--- app/src/main/java/com/example/calculaltorproject/domain/ext/SignsExt.kt
+++ app/src/main/java/com/example/calculaltorproject/domain/ext/SignsExt.kt
@@ -5,5 +5,5 @@ import com.example.calculaltorproject.domain.model.Signs
 
 fun Signs.toVisualRepresentation(): String = when (val action = this.action) {
     is Actions.Sign -> action.sign
-    is Actions.Number -> action.number
+    is Actions.Number -> action.numberSign
 }
