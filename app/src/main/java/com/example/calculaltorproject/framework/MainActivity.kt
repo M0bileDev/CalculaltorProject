7968cf521bbc5e702b165545631e4ddfbfe13e1f
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-10 00:49:32 +0200

    Support preview;

--- app/src/main/java/com/example/calculaltorproject/framework/MainActivity.kt
+++ app/src/main/java/com/example/calculaltorproject/framework/MainActivity.kt
@@ -4,40 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.calculaltorproject.domain.ext.groupNumbers
+import com.example.calculaltorproject.domain.ext.previewComposer
 import com.example.calculaltorproject.domain.ext.signComposer
 import com.example.calculaltorproject.domain.ext.textComposer
-import com.example.calculaltorproject.domain.ext.toVisualRepresentation
 import com.example.calculaltorproject.domain.model.Signs
 import com.example.calculaltorproject.presentation.screen.displayer.Displayer
 import com.example.calculaltorproject.presentation.screen.interactor.Interactor
@@ -67,12 +52,14 @@ fun CalculatorApp(modifier: Modifier = Modifier) {
             snackbarHost = { SnackbarHost(hostState = snackbarHostState) }) { innerPadding ->
 
             var displayValue by remember { mutableStateOf("") }
+            var previewResult by remember { mutableStateOf("") }
             val coroutine = rememberCoroutineScope()
 
             Column(modifier = Modifier.padding(innerPadding)) {
                 Displayer(
                     modifier = Modifier.weight(3f),
-                    value = displayValue,
+                    signsValue = displayValue,
+                    previewResultValue = previewResult,
                     onDeleteClicked = {
                         signsInUse.signComposer(
                             Signs.Delete,
@@ -80,6 +67,12 @@ fun CalculatorApp(modifier: Modifier = Modifier) {
                                 it.textComposer(onTextCompleted = { newText ->
                                     displayValue = newText
                                 })
+
+                                it.toMutableList()
+                                    .previewComposer(onPreviewCompleted = { newPreview ->
+                                        previewResult = newPreview
+                                    })
+
                             },
                             onSignError = {},
                             onError = {})
@@ -103,9 +96,18 @@ fun CalculatorApp(modifier: Modifier = Modifier) {
                                 signsInUse.signComposer(
                                     sign,
                                     onSignCompleted = {
+
+                                        //display numbers and signs
                                         it.textComposer(onTextCompleted = { newText ->
                                             displayValue = newText
                                         })
+
+                                        //display calculated value in the preview, below numbers and signs
+                                        it.toMutableList()
+                                            .previewComposer(onPreviewCompleted = { newPreview ->
+                                                previewResult = newPreview
+                                            })
+
                                     },
                                     onSignError = {
 
