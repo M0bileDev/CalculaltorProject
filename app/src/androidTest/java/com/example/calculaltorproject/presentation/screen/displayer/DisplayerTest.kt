2713823e995fee8c5a99c7099dc9e310b0929005
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-15 00:54:36 +0200

    add test for delete button;

--- app/src/androidTest/java/com/example/calculaltorproject/presentation/screen/displayer/DisplayerTest.kt
+++ app/src/androidTest/java/com/example/calculaltorproject/presentation/screen/displayer/DisplayerTest.kt
@@ -3,9 +3,12 @@ package com.example.calculaltorproject.presentation.screen.displayer
 import androidx.compose.ui.test.assertTextContains
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import com.example.calculaltorproject.domain.model.Actions
 import com.example.calculaltorproject.domain.model.Signs
 import com.example.calculaltorproject.presentation.theme.CalculaltorProjectTheme
+import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 
@@ -64,6 +67,34 @@ class DisplayerTest {
 
             return@with
 
+        }
+
+    @Test
+    fun givenInteractor_whenDeleteSignIsPressed_thenActionOnDeleteClickedIsExecuted() =
+        with(composeTestRule) {
+
+            val delete = Signs.Delete
+            val value = (delete.action as Actions.Sign).sign
+            var onDeleteClicked = false
+
+            //given
+            setContent {
+                Displayer(
+                    signsValue = "",
+                    previewResultValue = "",
+                    onDeleteClicked = {
+                        onDeleteClicked = !onDeleteClicked
+                    }
+                )
+            }
+
+            //when
+            onNodeWithText(value).assertExists().performClick()
+
+            //then
+            assertTrue(onDeleteClicked)
+
+            return@with
         }
 
 }
